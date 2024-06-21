0f62ef0a3595da0f20285b669fbcd61077fa1b94
Andre Licht <al@cloudeteer.de>
2024-06-21 15:10:53 +0200

    use TERRAFORM_OSS_AZURE_CLIENT_ID

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,10 +1,3 @@
-variable "github_token" {
-  description = "GitHub token to manage repositories"
-  type        = string
-  default     = ""
-  sensitive   = true
-}
-
 variable "create_repo" {
   description = "A repository name to create"
   type        = string
